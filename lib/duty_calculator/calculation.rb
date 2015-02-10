34095221b3187c9744442a3d95d30b2f28eae20c
Thomas Hanley <tom@walkerandcompany.com>
2015-02-10 14:21:49 -0800

    WIP

--- lib/duty_calculator/calculation.rb
+++ lib/duty_calculator/calculation.rb
@@ -4,15 +4,16 @@ module DutyCalculator
   class Calculation
     class << self
       def validate_params(params)
-        required = %w{from to shipping_mode classify_by shipping
+        required = %w{from to shipping_mode shipping
                       insurance currency preferential_rates detailed_result incl_hs_codes cat
                       hs country_of_hs_code desc
                       sku value weight qty origin reference}
         msg = []
         required.each do |r|
+          # ensure keys are strings
           msg << r unless params.inject({}){|memo,(k,v)| memo[k.to_s] = v; memo}.has_key?(r)
         end
-        raise ArgumentError, "Missing arguments from hash #{msg.to_s}" if msg.size
+        raise ArgumentError, "Missing arguments from hash #{msg.to_s}" if msg.size >= 1
         return params
       end
     end
@@ -47,12 +48,23 @@ module DutyCalculator
 #
 =end
     def self.get(params={})
-      uri = Addressable::URI.new
-      uri.query_values = validate_params(params)
-
+      qs = params.collect do |k,v|
+            if v.kind_of?(Array)
+              memo = ""
+              v.each_with_index do |value, index|
+                memo << "#{k}[#{index}]=#{value}"
+                memo << "&" if v.size > (index+1)
+              end
+              memo
+            else
+              "#{k}=#{v}"
+            end
+          end
       conn = DutyCalculator::Client.new
-      resp = conn.get "#{DutyCalculator::Client.api_base}/calculation?#{uri.query}"
-      resp.body
+      request = "#{DutyCalculator::Client.api_base}/calculation?#{qs.join('&')}"
+      raise Exception, "Duty Calculator Error: #{DutyCalculator::ErrorMessages.for_code(resp.body["error"]["code"])}" if resp.body["error"]
+      raise Exception, "HTTP Status Code #{resp.status}" if resp.status.to_i != Net::HTTPOK
+      return resp.body
     end
   end
 end
