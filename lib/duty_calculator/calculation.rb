17f197166994a0b658633f488c387bd504c0e6e1
Thomas Hanley <tom@walkerandcompany.com>
2015-03-10 17:04:32 -0700

    Fix response for raise error

--- lib/duty_calculator/calculation.rb
+++ lib/duty_calculator/calculation.rb
@@ -18,35 +18,6 @@ module DutyCalculator
       end
     end
 
-=begin
-# https://docs.google.com/a/walkerandcobrands.com/document/d/16e_lh7MIjLGYvS_qZL7pUzOvTVte-cqTnr4jGo_VtIM/pub
-# from={ISO  alpha-3 country code or alpha-2 country code} \
-# &to={ISO  alpha-3 country code or alpha-2 country code} \
-# &province={ISO alpha-2 province code of ‘importing to’ country (only required for Canada and Brazil)} \
-# &shipping_mode={shipping mode} \
-# &commercial_importer={importer status (only required for Russia)} \
-# &imported_wt={total weight of products imported during the month (only required for Russia)} \
-# &imported_value={total value of products imported during the month (only required for Russia)} \
-# &classify_by={items classification identifier} \
-# &cat[0]={item ID or category ID} \
-# &hs[0]={item HS code} \
-# &country_of_hs_code[0]={ISO  alpha-3 country code or alpha-2 country code} \
-# &desc[0]={product description} \
-# &sku[0]={item sku } \
-# &value[0]={value per one item} \
-# &weight[0]={weight of one item} \
-# &qty[0]={item quantity} \
-# &origin[0]={ISO alpha-3 country code or alpha-2 country code} \
-# &reference[0]={product reference}  \
-# &shipping={shipping cost} \
-# &insurance={cost of insurance} \
-# &currency={ISO currency code} \preferential
-# &output_currency={ISO currency code} \
-# &preferential_rates={1 to apply FTA and preferential rates, 0 to ignore FTA and preferential rates} \
-# &detailed_result={1 for detailed result, 0 for short result} \
-# &incl_hs_codes={include HS code for each item in response}
-#
-=end
     def self.get(params={})
       qs = params.collect do |k,v|
             if v.kind_of?(Array)
@@ -61,9 +32,9 @@ module DutyCalculator
             end
           end
       conn = DutyCalculator::Client.new
-      request = "#{DutyCalculator::Client.api_base}/calculation?#{qs.join('&')}"
+      resp = conn.get "#{DutyCalculator::Client.api_base}/calculation?#{qs.join('&')}"
       raise Exception, "Duty Calculator Error: #{DutyCalculator::ErrorMessages.for_code(resp.body["error"]["code"])}" if resp.body["error"]
-      raise Exception, "HTTP Status Code #{resp.status}" if resp.status.to_i != Net::HTTPOK
+      raise Exception, "HTTP Status Code #{resp.status}" if resp.status.to_i != 200
       return resp.body
     end
   end
